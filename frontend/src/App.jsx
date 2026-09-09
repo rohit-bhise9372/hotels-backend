038c1262c127aa60ba00ccc510ed550bdd139d98
Rohit Bhise <rohitbhise9372@gmail.com>
2026-09-09 16:39:17 +0530

    Update App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import AllHotels from "../components/AllHotels";
-import HotelByName from "../components/HotelByName";
+import AllHotels from "./components/AllHotels";
+import HotelByName from "./components/HotelByName";
 
 function App() {
   return (
